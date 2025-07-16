04a4bcae6048d0bf75743340d3cbec187feb71a7
dibyajyoti nayak <dibyajyotianshuman_nayak@apple.com>
2025-07-16 01:20:30 -0700

    first commit

---
+++ dbtlearn/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized = 'view'
+        )
+}}
+with src_hosts as (
+    select * 
+    from {{ref("src_hosts")}}
+)
+select
+HOST_ID,
+nvl(HOST_NAME,'Anonymous') as HOST_NAME,
+IS_SUPERHOST,
+CREATED_AT,
+UPDATED_AT
+FROM src_hosts
